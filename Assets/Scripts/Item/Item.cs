4322679ccf6e10a30a9899897b6974c67fa28a65
Samuel Figueiredo Veronez <83087877+Zeronevi@users.noreply.github.com>
2025-01-09 14:59:27 -0300

    Remove Items
    Items are out of the scope of the first delivery.

--- Assets/Scripts/Item/Item.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public abstract class Item:ScriptableObject{
-    public Sprite itemIcon;
-    public string itemName;
-    
-    public uint itemRank;
-    public int itemPrice;
-}
