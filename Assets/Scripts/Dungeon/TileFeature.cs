3ea97d0702ad441533e18e705c946845f3109871
Samuel Figueiredo Veronez <83087877+Zeronevi@users.noreply.github.com>
2025-04-03 14:03:10 -0300

    Update TileFeature.cs

--- Assets/Scripts/Dungeon/TileFeature.cs
+++ Assets/Scripts/Dungeon/TileFeature.cs
@@ -1,5 +1,10 @@
+using UnityEngine;
+
 public abstract class TileFeature
 {
     public abstract void Activate(PlayerSave playerSave);
     public abstract void Deactivate(PlayerSave playerSave);
+
+    public abstract void OnCreate(GameObject tileGameObject);
+    public abstract void OnDestroy(GameObject tileGameObject);
 }
